03b500b7540ff5e2434641b73078c2cf05c60a5a
Sachin Singh <sachinsingh9272@gmail.com>
2025-04-07 22:32:19 +0530

    Created Header Component

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,21 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  return(
+    <main className="flex min-h-screen flex-col lg:flex-row">
+    {/* Visual Side - only visible on large screens */}
+    <aside className="hidden lg:flex w-full lg:w-1/2 bg-gray-100 items-center justify-center">
+      <img
+        src="/your-image.jpg" // Replace with your actual image
+        alt="Welcome visual"
+        className="object-cover w-full h-full"
+      />
+    </aside>
+
+    {/* Auth Side */}
+    <section className="w-full lg:w-1/2 flex items-center justify-center bg-white p-6">
+      <SignIn/>
+    </section>
+  </main>
+  )
 }
--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from './_component/Header'
 
 const DashboardLayout = ({children}) => {
   return (
     <div>
+      <Header/>
       {children}
     </div>
   )
