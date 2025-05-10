cf052ed3f563443b29d181bcc66b2962cb9994d7
Sachin Singh <sachinsingh9272@gmail.com>
2025-05-10 23:34:42 +0530

    completed entire question section

--- app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
@@ -1,17 +1,28 @@
-import { Lightbulb } from 'lucide-react';
+import { Lightbulb, Volume2 } from 'lucide-react';
 import React from 'react'
 
 const QuestionSection = ({mockInterviewQuestion, activeQuestionIndex}) => {
     console.log("Mock Interview",mockInterviewQuestion);
     console.log("active", activeQuestionIndex)
+    const textToSpeech = (text) => {
+      if('speechSynthesis' in window){
+        const speech = new SpeechSynthesisUtterance(text)
+        window.speechSynthesis.speak(speech)
+      }else{
+        alert('Sorry, Your browser does not support text to speech')
+      }
+    }
   return mockInterviewQuestion &&(
     <div className='p-5 border rounded-lg my-10'>
       <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
         {mockInterviewQuestion&&mockInterviewQuestion.map((question,index)=>(
-            <h2 className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestionIndex == index && 'bg-[#4845D2] text-white'} `} key={question.id || index}>Question #{index+1}</h2>
+            <h2 className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestionIndex == index && 'bg-blue-700 text-white text-lg'} `} key={question.id || index}>Question #{index+1}</h2>
         ))}
       </div>
       <h2 className='my-5 text-md md:text-lg'>{mockInterviewQuestion[activeQuestionIndex]?.question}</h2>
+
+        <Volume2 className='cursor-pointer' onClick={()=>textToSpeech(mockInterviewQuestion[activeQuestionIndex]?.question)}/>
+
       <div className='border rounded-lg p-5 bg-blue-100 mt-20'>
         <h2 className='flex gap-2 items-center text-primary'>
             <Lightbulb/>
