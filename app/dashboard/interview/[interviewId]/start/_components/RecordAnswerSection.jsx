20fc7eebea3b7655920ddd9a1a766c31dc9d492c
Sachin Singh <sachinsingh9272@gmail.com>
2025-05-15 23:47:11 +0530

    Saved user response generate rating based on response

--- app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
@@ -5,9 +5,10 @@ import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import useSpeechToText from 'react-hook-speech-to-text';
 import { Mic } from 'lucide-react'
+import { toast } from 'sonner';
+import { ChatSession } from '@google/generative-ai'
 
-
-const RecordAnswerSection = () => {
+const RecordAnswerSection = ({mockInterviewQuestion, activeQuestionIndex}) => {
   const [userAnswer, setUserAnswer] = useState('');
   const {
     error,
@@ -25,6 +26,23 @@ const RecordAnswerSection = () => {
       setUserAnswer(prevAns=>prevAns+result?.transcript)
     })
   }, [results])
+
+  const SavedUserAnswer = async() => {
+    if(isRecording){
+      stopSpeechToText();
+      if(userAnswer?.length < 10){
+        toast('Error while saving your answer, Please record again')
+        return;
+      }
+      const feedbackPrompt = "Question:"+mockInterviewQuestion[activeQuestionIndex]?.question + ", User Answer:"+userAnswer+",Depends on question and user answer for give interview question"+" please give us rating for answer and feedback as area of improvement if any"+" in just 3 to 5 lines to improve it in JSON format with rating field and feedback field";
+      const result = await ChatSession.sendMessage(feedbackPrompt);
+      console.log("Feedback Result--->", result);
+      const mockJsonResp = (result.response.text()).replace(/```json|```/g, '').trim();
+      console.log(mockJsonResp);
+    }else{
+      startSpeechToText();
+    }
+  }
   
   return (
     <div className='flex items-center justify-center flex-col'>
@@ -48,7 +66,7 @@ const RecordAnswerSection = () => {
         :
       'Record Answer' }
     </Button>
-    <Button onClick={()=>console.log(userAnswer)}>Show User Answer</Button>
+    <Button onClick={()=>console.log("show user ans---->",userAnswer)}>Show User Answer</Button>
     </div>
   )
 }
